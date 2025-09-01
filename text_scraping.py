accbe4e24c2584bb87a255bbc20ff3c71a9826d3
LucaDiliee <luca.dilie@gsom.polimi.it>
2025-09-01 14:11:35 +0200

    Update text_scraping.py

--- text_scraping.py
+++ text_scraping.py
@@ -152,7 +152,7 @@ def main():
     parser.add_argument("--out-bigrams", default="keywords_bigrams.csv", help="CSV output bigrammi")
     args = parser.parse_args()
 
-    root = Path("6.16")
+    root = Path("news")
     
     if not root.exists():
         raise SystemExit(f"Cartella non trovata: {root}")
